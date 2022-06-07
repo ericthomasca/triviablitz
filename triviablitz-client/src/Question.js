df0beddd70631ea7ee57ecce4cb779b2048334c8
Eric Thomas <ericthomas@tuta.io>
2022-06-07 16:47:22 -0230

    fetch command and spelling

--- triviablitz-client/src/Question.js
+++ triviablitz-client/src/Question.js
@@ -1,12 +1,21 @@
 import { useState, useEffect } from 'react';
 
 
-export function Game() {
+export function Question() {
 
     const [question, setQuestion] = useState(null);
     const difficulty = 'easy'
     const API = 'https://opentdb.com/api.php?amount=1&type=multiple&difficulty=' + difficulty;
 
+    useEffect(() => {
+        const fetchData = async () => {
+          const result = await fetch(API);
+          const body = await result.json();
+          setQuestion(body);
+        };
+        fetchData();
+      }, [API]);
+
 }
 
-export default Game;
+export default Question;
