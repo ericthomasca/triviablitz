8b667512f3dc83e4a1805365ae6823196227ac61
Eric Thomas <ericthomas@tuta.io>
2022-06-12 14:01:59 -0230

    refactor HomePage

--- triviablitz-client/src/App.js
+++ triviablitz-client/src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Question, HomePage} from "./Question";
-
-
-
+import HomePage from "./HomePage";
+import Question from "./Question";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>}></Route>
-        <Route path="/game" element={<Question questionDifficulty={"easy"}/>}></Route>
-
+        <Route path="/" element={<HomePage />}></Route>
+        <Route
+          path="/game"
+          element={<Question questionDifficulty={"easy"} />}
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
