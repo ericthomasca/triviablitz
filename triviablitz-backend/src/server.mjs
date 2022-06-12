9e4d41b6b80a5a21795f0c6b5b6d3b2b30ed4850
DawsonMercer <dwem52@mun.ca>
2022-06-12 18:03:52 -0230

    adding user to database - there are bugs

--- triviablitz-backend/src/server.mjs
+++ triviablitz-backend/src/server.mjs
@@ -1,35 +1,39 @@
 import express from "express";
-import {MongoClient} from "mongodb";
+import { MongoClient } from "mongodb";
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/hello', (req, res) => res.send("Hello Stephen"));
+app.get("/hello", (req, res) => res.send("Hello Stephen"));
 
-app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`))
+app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
 
-app.post('/hello', (req, res) =>{
-    res.send(`Hello ${req.body.name} this is a POST request`);
+app.post("/hello", (req, res) => {
+  res.send(`Hello ${req.body.name} this is a POST request`);
 });
 
-app.post('/api/addUser', async(req, res) =>{
-    try{
-        await client.connect();
+app.post("/api/addUser", async (req, res) => {
+  try {
+    await client.connect();
 
-        const db = client.db('users');
-        const newUser = 
+    const db = client.db("users");
+    const newUser = await db
+      .collection("users")
+      .insertOne({
+        user: req.body.user,
+        score: req.body.score,
+        time_last_played: req.body.time_last_played,
+      });
+    // cookie: req.body.cookie
 
-        res.sendStatus(200);
+    res.sendStatus(200);
 
-        client.close();
-    }
-    catch(error){
-        console.log(error);
-        res.sendStatus(500);
-        
-    }
-})
+    client.close();
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 
-
-app.listen(8000, () => console.log('listening on port 8000'));
+app.listen(8000, () => console.log("listening on port 8000"));
