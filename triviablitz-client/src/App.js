833d3f2f58db1e3cb71f6a2ce1aeffcd660616ec
DawsonMercer <dwem52@mun.ca>
2022-06-19 14:55:15 -0230

    i messed up

--- triviablitz-client/src/App.js
+++ triviablitz-client/src/App.js
@@ -1,21 +1,28 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
-import Question from "./Question";
+import QuestionPage from "./Question";
+import MyTimer from "./Timer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
 
+
 function App() {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route
           path="/game"
-          element={<Question questionDifficulty={"easy"} />}
+          element={<QuestionPage />}
         ></Route>
+        <Route path="/timer"
+        element={<MyTimer />}></Route>
       </Routes>
     </BrowserRouter>
   );
